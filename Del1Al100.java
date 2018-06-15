293fbadf35d8e89d4c118631899404d75615bec7
Juan José García <39605602+JuanJo193@users.noreply.github.com>
2018-06-14 20:06:26 -0500

    Add files via upload

---
+++ Del1Al100.java
@@ -0,0 +1,18 @@
+
+package del1al100;
+
+import java.util.Scanner;
+
+public class Del1Al100 {
+
+   
+    public static void main(String[] args) {
+       
+        int a = 1;
+        
+        while (a<=100){
+            System.out.println(a);
+            a++;
+    }  
+    }
+}
